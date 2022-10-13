84eae3a5b32f2d881044e328b8d181c45559b076
Jonathan <gobolijonathan12@yahoo.com>
2022-10-13 19:53:19 +0800

    Initialized Array folder

---
+++ Array/js/array.js
@@ -0,0 +1,18 @@
+//Array
+
+const sampleArray = ['a', 'b', 'c', 'd'];
+
+//methods to get the value
+sampleArray[0];
+sampleArray[1];
+sampleArray[2];
+sampleArray[3];
+
+//sample methods in array
+
+/* Add 1 value to the last line of the array */
+sampleArray.push('e ');
+/* Remove the last value inside the array */
+sampleArray.pop();
+/* Add value in the first part of the array */
+sampleArray.unshift();
