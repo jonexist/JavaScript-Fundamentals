4cb9e5a8306894be1d407e6cb3719579bc1f9404
Jonathan <gobolijonathan12@yahoo.com>
2022-10-13 19:54:06 +0800

    Initialized Condition folder

---
+++ Condition/js/condition.js
@@ -0,0 +1,23 @@
+//Conditions
+
+/* Using (if/else) */
+let name = 'Jonathan';
+
+if (typeof name =='string') {
+  console.log('YES');
+} else {
+  console.log('NO');
+}
+//end
+
+/* Using switch */
+let age = 21;
+
+switch (age) {
+  case 21:
+    console.log('Twenty One'); break;
+  case 20:
+    console.log('Twenty'); break;
+  default:
+    console.log('None'); 
+}
